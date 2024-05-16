a75927781e172eb2858a76cf514421eb760e612a
kentechn <cdspyk9236@gmail.com>
2024-05-17 08:04:50 +0900

    taskエンドポイント、スキーマを追加

--- app/src/api/endpoints/tasks.py
+++ app/src/api/endpoints/tasks.py
@@ -1,33 +1,42 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Query, status
+
+from src.schemas.task import (
+  CreateTask,
+  TaskResponse,
+  TasksResponse,
+  UpdateTask,
+)
 
 router = APIRouter()
 
 
-@router.get("/", response_model=list[dict])
-def read_tasks() -> list[dict]:
-  # Logic to fetch all tasks from the database
+@router.get("/", response_model=TasksResponse, response_model_exclude_unset=True)
+def read_tasks(q: Annotated[str | None, Query(max_length=50)] = None) -> TasksResponse:
+  # Logic to fetch all tasks from the database using query_params
   pass
 
 
-@router.get("/{task_id}", response_model=dict)
-def read_task(task_id: int) -> dict:
+@router.get("/{task_id}", response_model=TaskResponse)
+def read_task(task_id: int) -> TaskResponse:
   # Logic to fetch a specific task by task_id from the database
   pass
 
 
-@router.post("/", response_model=dict)
-def create_task(task: dict) -> dict:
+@router.post("/", response_model=TaskResponse, status_code=status.HTTP_201_CREATED)
+def create_task(task: CreateTask) -> TaskResponse:
   # Logic to create a new task in the database
   pass
 
 
-@router.put("/{task_id}", response_model=dict)
-def update_task(task_id: int, task: dict) -> dict:
+@router.put("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def update_task(task_id: int, task: UpdateTask) -> None:
   # Logic to update a specific task by task_id in the database
   pass
 
 
-@router.delete("/{task_id}", response_model=dict)
-def delete_task(task_id: int) -> dict:
+@router.delete("/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: int) -> None:
   # Logic to delete a specific task by task_id from the database
   pass
