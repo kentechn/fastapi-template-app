9d2f121edc104749fb5c5cb0f065df6543de82c1
kentechn <cdspyk9236@gmail.com>
2024-05-25 12:08:09 +0900

    テストコミット

--- app/src/core/database.py
+++ app/src/core/database.py
@@ -5,6 +5,5 @@ from src.config import get_settings
 
 settings = get_settings()
 
-
 engine = create_engine(settings.mysql_db_url, echo=True)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
